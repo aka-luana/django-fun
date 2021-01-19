9d3de17fe70669b59b8ccf3c6aba69f1d124d71a
unknown <Python@BUE302D16.alunos.sc.senac.br>
2021-01-19 17:23:30 -0300

    Django feito em sala.

--- example/models.py
+++ example/models.py
@@ -1,6 +1,23 @@
 from django.db import models
 
 class Person(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    age = models.IntegerField(default=None, blank=True)
+    id = models.AutoField(primary_key=True)
+    SHIRT_SIZES = (
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    )
+    
+    first_name = models.CharField("person's first name", max_length=30)
+    last_name = models.CharField("person's last name", max_length=30)
+    age = models.IntegerField("person's age", default=None, blank=True)
+    shirt_size = models.CharField("person's shirt size", max_length=1, choices=SHIRT_SIZES, default=None, blank=True)
+
+class Musician(Person):
+    instrument = models.CharField(max_length=100)
+
+class Album(models.Model):
+    artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    release_date = models.DateField()
+    num_stars = models.IntegerField()
