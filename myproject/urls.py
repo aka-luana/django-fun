f3724fad0a215b8497c75db9233dab4344c5d51d
unknown <Python@BUE302D16.alunos.sc.senac.br>
2021-01-21 17:26:44 -0300

    inicio login

--- myproject/urls.py
+++ myproject/urls.py
@@ -13,10 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
+
 from django.contrib import admin
 from django.urls import path, include
 
+from accounts import views as accounts_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pokemaoc/', include('pokemaoc.urls'))
+    path('pokemaoc/', include('pokemaoc.urls')),
+    path('signup/', accounts_views.signup, name='signup'),
+    # url(r'^signup/$', accounts_views.signup, name='signup'),
+    # url('accounts/', accounts_views.signup, name='signup'),
 ]
